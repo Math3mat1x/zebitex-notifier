c31e5809c9f0da6b9ed10411e2a9a0bd4bf34df0
Math3mat1x <maxime.corne@protonmail.com>
2022-04-06 18:18:50 +0200

    main.py - support for multiple markets at the time

--- main.py
+++ main.py
@@ -11,23 +11,23 @@ def zebitex_price(market="btceur"):
 with open("data.json", "r") as f:
     data = json.loads(f.read())
 
-market = "btceur"
-
-high = data[market][0]
-low  = data[market][1]
-done = data[market][2]
-
-if not done:
-    price = zebitex_price(market)
-    if price >= high:
-        send("Price of {} is high!".format(market))
-        done = True
-    elif price <= low:
-        send("Price of {} is low!".format(market))
-        done = True
-
-if done:
-    data[market] = [high, low, done]
-
-    with open("data.json", "w") as f:
-        f.write(json.dumps(data))
+markets = list(data.keys())
+for market in markets:
+    high = data[market][0]
+    low  = data[market][1]
+    done = data[market][2]
+    
+    if not done:
+        price = zebitex_price(market)
+        if price >= high:
+            send("Price of {} is high!".format(market))
+            done = True
+        elif price <= low:
+            send("Price of {} is low!".format(market))
+            done = True
+    
+    if done:
+        data[market] = [high, low, done]
+    
+        with open("data.json", "w") as f:
+            f.write(json.dumps(data))
