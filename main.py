249dd83756f6ef9f67ab24ded72a1d0fe9736f9e
Math3mat1x <maxime.corne@protonmail.com>
2022-04-06 18:10:57 +0200

    added main functionnality

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import api
+import json
 from credentials import access_key, private_key
+from message import send
 
 def zebitex_price(market="btceur"):
     client = api.Zebitex(access_key, private_key, False)
     response = client.public_trade_history(market).json()
     return float(response[0]["price"])
+
+with open("data.json", "r") as f:
+    data = json.loads(f.read())
+
+market = "btceur"
+
+high = data[market][0]
+low  = data[market][1]
+done = data[market][2]
+
+if not done:
+    price = zebitex_price(market)
+    if price >= high:
+        send("Price of {} is high!".format(market))
+        done = True
+    elif price <= low:
+        send("Price of {} is low!".format(market))
+        done = True
+
+if done:
+    data[market] = [high, low, done]
+
+    with open("data.json", "w") as f:
+        f.write(json.dumps(data))
---
+++ message.py
@@ -0,0 +1,4 @@
+def send(*args):
+    """Custom function to send a notification."""
+
+    print(args)
